53060a1d41242e3b7da22824534f220bc68548af
Lillie Schachter <lillieschachter@gmail.com>
2022-09-06 19:53:33 -0500

    abandoning. pushing for posterity.

--- nba_stats.py
+++ nba_stats.py
@@ -1,60 +1,48 @@
 
 def three_pointer_calculations():
+    player_a = 'Jamal Murray'
+    player_b = 'Fred VanVleet'
+    player_c = 'James Harden'
+
     # In the 2020 NBA playoffs, Jamal Murray, Fred VanVleet, and James Harden rank
     # #1, #2, and #3 respectively for the number of 3-point shots made at 46, 43, and 37.
 
     # 1.1 Create variables to track the number of 3-point shots that
     # Jamal Murray, Fred VanVleet and James Harden have made.
 
-    # 1.2 Print out the number of three point shots using 
-    # f-strings with the variables you created for each player
+    # 1.2 Print out the number of three point shots per person using 
+    # f-strings with the variables created above for each player, for the result:
+    # 'Person A: x, Person B: y, Person C: z'
 
     # In the 2020 NBA playoffs, Jamal Murray, Fred VanVleet, and James Harden attempted
     # 93, 110, 109 three point shots total.
     
     # 1.3 Create variables to store the values above
 
-    # Build on your print statement!
-    
-    # 1.4 Copy the print statements you wrote above in 1.2 and extend them by:
-    # Printing both the number of three point shots each player made AND 
-    # the number of three point shots each player attempted. 
-    # Use only one print() statement for each player 
-    # (remember that you can use 'f-strings' to insert variables into lines of text 
-    # (reference the examples above if you forgot how to do this).
-
-    # 1.5 Calculate and print the three point percentage for each player
-    # (The three point percentage is given by the following formula: 
-    # 3 point shots made/3 point shots attempted)
+    # 1.4 Calculate and print the three point percentage for each player, rounded to 2 decimal points.
+    # The three point percentage is given by the following formula: 
+    # 3-point shots made/3-point shots attempted
+    # Can you turn that into a percentage? Ala:
+    # "Player A's three point percentage is d%. Player B's three point percentage is e%. Player C's three point percentage is f%."
 
 
 def process_laker_data():
-    # Below is a big paragraph of text about the Lakers 2020 season from
-    # https://www.lineups.com/nba/roster/los-angeles-lakers
-
-    # The Lakers went all in this offseason and swung a deal for former Pelicans forward Anthony Davis. They sent a package of Brandon Ingram, Lonzo Ball, Josh Hart, and 3 first-round picks to New Orleans to land Davis. Those three have made good developments with the Pelicans, especially Brandon Ingram. But, the deal is still a huge win for the Lakers as Lebron, Davis, and company have put together an incredible season. Los Angeles has ridden James and Davis, along with a supporting cast built around them, to the second-best record in the NBA. The Lakers ended the season atop the Western Conference with a record of 49-14. They were narrowly behind the Bucks for the best record in the league. Davis proved to the final piece necessary for the Lakers to rebound from missing the playoffís last year. Los Angeles was a dominant club on both sides of the ball and are in a position to have another successful year next season.
-
-    # This is a HUGE chunk of text. 
-
-    # 2.1 Can you add escape characters to print out this text from a python script to the command line
-    # so that only one sentence is on each line?
-
     # Are the Lakers the best team?
 
-    # 2.2 Make a boolean variable called lakers_are_best that indicates, to the best of your knowledge, 
+    # 2.1 Make a boolean variable called are_lakers_best that indicates, to the best of your knowledge, 
     # whether the following statement is true: The Lakers are the best basketball team in the NBA
 
-    # 2.3 Using an f-string containing your lakers_are_best variable, print out your evaluation of 
+    # 2.2 Using an f-string containing your are_lakers_best variable, print out your evaluation of 
     # whether the statement was true or not
 
-    # 2.4 Convert your lakers_are_best variable to an integer, and print it out. 
+    # 2.3 Convert your are_lakers_best variable to an integer, and print it out. 
     # Why do you think it takes this value?
 
-    # 2.5 Convert your lakers_are best variable to a float, and print it out
+    # 2.4 Convert your are_lakers_best variable to a float, and print it out
 
-    # 2.6 Take each player's three point percentage from above, convert it to a string, then print it out.
+    # 2.5 Take each player's three point percentage from above, convert it to a string, then print it out.
     # What do you notice?
 
-    # 2.7 Take each player's three point percentage (from part 2.5) and convert it to an integer, 
+    # 2.6 Take each player's three point percentage (from part 2.5) and convert it to an integer, 
     # then print it out.
     # What do you notice?
